03906d1a096b3e9d309585226acee66ebc7629cb
Lauren Chan <chanlc@umich.edu>
2026-02-23 09:16:06 +0100

    Render notes with v-for

--- app.js
+++ app.js
@@ -30,6 +30,10 @@ Vue.createApp({
         //
         // In Commit 4:
         // - Replace this with a call to loadFromStorage().
+        this.stickies = [
+            { id: 1, text: "Test note 1" },
+            { id: 2, text: "Another note" }
+        ];
     },
 
     methods: {
